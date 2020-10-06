2accdbe61219e99e5df41aa5f33183a3461eacf4
Eddyjamin <eddyjamin@gmail.com>
2020-10-06 02:31:51 +0100

    http
    added http dependency

--- lib/card.dart
+++ lib/card.dart
@@ -5,12 +5,14 @@ import 'package:hello_flutter/pages/home_pages.dart';
 main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Homepage(),
-    theme: ThemeData(primarySwatch: Colors.teal),
-  );
+      home: Homepage(),
+      theme: ThemeData(primarySwatch: Colors.teal),
+    );
+  }
 }
 //runApp(MyApp) -> extends stle -> returns Material
